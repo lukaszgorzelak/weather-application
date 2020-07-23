530c9e6d9c88d146b54c85a17ac379cbc9cad4c0
Łukasz <lukaszgorzelak91@gmail.com>
2020-07-23 19:20:34 +0200

    Updates

---
+++ precache-manifest.507d3048a64c771c5e5a1bc7c9fc936a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4739b7f8f79b73ffbc9583982a9140f",
+    "url": "/weather-application/index.html"
+  },
+  {
+    "revision": "e724c25863a65e843dbb",
+    "url": "/weather-application/static/css/main.7c066b46.chunk.css"
+  },
+  {
+    "revision": "2ba9787abd111f8e6d61",
+    "url": "/weather-application/static/js/2.7bb4a6d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-application/static/js/2.7bb4a6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e724c25863a65e843dbb",
+    "url": "/weather-application/static/js/main.fd0a6df6.chunk.js"
+  },
+  {
+    "revision": "61fc5595b17fa55b2954",
+    "url": "/weather-application/static/js/runtime-main.6c548421.js"
+  }
+]);
